d42328186c32bf29fab61da8b72651729aee63b8
Rishita37 <108847099+Rishita37@users.noreply.github.com>
2022-08-01 20:24:50 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,28 @@
 import geni.portal as portal
-import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
+         
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
 
-pc = portal.Context()
-request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="bash /local/repository/silly.sh"))
-
+# Create a XenVM
+for i in range(2):
+  if i == 0:
+    node = request.XenVM("webserver")
+  else: 
+    node = request.XenVM("observer")
+    
+  node.routable_control_ip = "true"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  if i == 0:
+    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
+    
 # Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+portal.context.printRequestRSpec()
