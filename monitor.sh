b10a352785358b4a41732904baef9fea7e581379
Rishita37 <108847099+Rishita37@users.noreply.github.com>
2022-08-03 23:46:12 -0400

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+old=$(head -1 /var/webserver_monitor/count.txt)
+latest=$(wc -l < /var/webserver_monitor/unauthorized.log)
+lines=$(($latest - $old))
+if((lines>0))
+then
+  tail -n $lines /var/webserver_monitor/unauthorized.log | mail -s "New entries" testwestchester3@gmail.com 
+else
+  echo "No unauthorized access" | mail -s "No unauthorized access" testwestchester3@gmail.com
+fi
+$newLines > /var/webserver_monitor/count.txt 
