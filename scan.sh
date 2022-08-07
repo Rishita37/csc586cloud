6c1a4fdf78cd2c19ab17dbe0b2fe102af30af129
Rishita37 <108847099+Rishita37@users.noreply.github.com>
2022-08-07 17:16:04 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-ips=$(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $10}')
-months=$(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $1}')
-days=$(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $2}')
+
+apt-get install whois -y
+
+ips=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $10}')
+months=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $1}')
+days=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $2}')
 list_ips=()
 list_months=()
 list_days=()
@@ -15,10 +18,12 @@ done
 for date in $days; do
     list_days+=($date)
 done
+countries=()
 for ip in $ips; do
-    country=$(curl ipinfo.io/$ip/country)
-    countries+=${country}
-done    
-for ip in ${!countries[*]}; do
-    echo "${list_ips[ip]}  ${countries[ip]} ${list_months[ip]} ${list_days[ip]}" >> /var/webserver_log/unauthorized.log
+    country=$(whois $(curl ifconfig.me) | grep -iE ^country | awk '{print $2}')
+    countries+=($country)
 done
+for ip in ${!countries[*]}; do
+    echo "${list_ips[ip]}  ${countries[ip]}  ${list_months[ip]}  ${list_days[ip]}" >> /var/webserver_log/unauthorized.log
+done
+
